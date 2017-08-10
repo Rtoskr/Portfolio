21eb80d3648a0870c4919debc4639e1e3b2751fa
Eddie Carter <30909842+Rtoskr@users.noreply.github.com>
2017-08-10 19:37:50 -0400

    Added tracking for page load times.

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using StackExchange.Profiling;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,22 +10,9 @@ namespace Web.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -6,11 +6,27 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using StackExchange.Profiling;
+using System.Diagnostics;
 
 namespace Web
 {
+    public static class Globals
+    {
+        public static Stopwatch RequestTimer = new Stopwatch();
+    }
     public class MvcApplication : System.Web.HttpApplication
     {
+        protected void Application_BeginRequest()
+        {
+            Globals.RequestTimer.Start();
+        }
+
+        protected void Application_EndRequest()
+        {
+            Globals.RequestTimer.Reset();
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
